7c86a5e8880983049a12624a98c460541d455c9e
Eduardo <eduardomacielbr@gmail.com>
2022-06-09 00:42:39 -0300

    Updating JSON read file function

--- src/utils/about.ts
+++ src/utils/about.ts
@@ -4,7 +4,7 @@ import path from 'path'
 export async function getAboutData() {
     const filePath = path.join(process.cwd(), '/src/utils/data/about-info.json');
     const jsonData = await fsPromises.readFile(filePath);
-    const objectData = JSON.parse(jsonData);
+    const objectData = JSON.parse(jsonData.toString());
 
     return objectData;
 }
